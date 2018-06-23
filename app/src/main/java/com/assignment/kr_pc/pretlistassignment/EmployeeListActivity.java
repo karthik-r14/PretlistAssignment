ad96826af92e4e337ba5a1dd721f93dcd6a8022f
Karthik R <karthikr1493@gmail.com>
2018-06-23 23:09:40 +0530

    Reduced font size of emp details.

--- app/src/main/java/com/assignment/kr_pc/pretlistassignment/EmployeeListActivity.java
+++ app/src/main/java/com/assignment/kr_pc/pretlistassignment/EmployeeListActivity.java
@@ -9,11 +9,13 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.Toast;
+
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 
 public class EmployeeListActivity extends AppCompatActivity {
@@ -44,7 +46,6 @@ public class EmployeeListActivity extends AppCompatActivity {
                     Employee employee = postSnapshot.getValue(Employee.class);
                     //adding employee to the list
                     employeeList.add(employee);
-                    Toast.makeText(getApplicationContext(), employee.getEmpDept(), Toast.LENGTH_SHORT).show();
                 }
 
                 LinearLayoutManager layoutManager = new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false);
