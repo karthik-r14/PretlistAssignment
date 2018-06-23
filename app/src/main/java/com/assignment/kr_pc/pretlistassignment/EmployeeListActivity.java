2af6b83e37cc89dc0cc2c573938dea824e385584
Karthik R <karthikr1493@gmail.com>
2018-06-23 14:31:52 +0530

    Created to activities WifiInfoActivity and EmployeeListActivity which get invoked on button clicks.

---
+++ app/src/main/java/com/assignment/kr_pc/pretlistassignment/EmployeeListActivity.java
@@ -0,0 +1,13 @@
+package com.assignment.kr_pc.pretlistassignment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EmployeeListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee_list);
+    }
+}
--- app/src/main/java/com/assignment/kr_pc/pretlistassignment/MainActivity.java
+++ app/src/main/java/com/assignment/kr_pc/pretlistassignment/MainActivity.java
@@ -1,7 +1,9 @@
 package com.assignment.kr_pc.pretlistassignment;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onProblemOneClick(View view) {
+        Intent intent = new Intent(MainActivity.this, EmployeeListActivity.class);
+        startActivity(intent);
+    }
+
+    public void onProblemTwoClick(View view) {
+        Intent intent = new Intent(MainActivity.this, WifiInfoActivity.class);
+        startActivity(intent);
     }
 }
